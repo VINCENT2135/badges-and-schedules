4973ff4240db3924acc99ce370707d8f1f03bed4
Vincent Ivos <vincentivos1@gmail.com>
2020-11-16 14:43:15 -0500

    commit tree f86c7f0828142d6351f1625ebfb657d01ff45544

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
+def badge_maker(n)
+    return "Hello, my name is #{n}."
+  end
+  
+  def batch_badge_creator(sp)
+    # returns an array of badge messages, using badge_maker
+    messages = []
+    sp.each do |item|
+      message = badge_maker(item)
+      messages.push(message)
+    end
+    messages
+  end
+  
+
+  
+def assign_rooms(sp)
+    count =0
+    sp.collect do |item|
+      count +=1
+      "Hello, #{item}! You'll be assigned to room #{count}!"
+    end
+  end
+  
+ 
+def printer(sp)
+batch_badge_creator(sp).each do |i|
+puts  i
+    end
+    
+assign_rooms(sp ).each do |i|
+puts i
+    end
+  end
+
 # Write your code here.
